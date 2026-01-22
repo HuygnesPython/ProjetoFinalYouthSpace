181d64ecf44fd63f029c8c7bc27809108c16bf33
HuygnesPython <huygneszin@gmail.com>
2026-01-21 21:52:37 -0300

    0.6.0

--- app.py
+++ app.py
@@ -1,8 +1,21 @@
-import mysql.connector
+import mysql.connector as my
 from flask import *
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+def conectar():
+    return my.connect(
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        port=os.getenv("DB_PORT"),
+        database=os.getenv("DB_NAME")
+        
+    )
 
 app = Flask(__name__)
-
+app.secret_key = os.getenv("FLASK_SECRET_KEY")
 @app.route('/')
 def index():
     title = 'Pagina inicial'
