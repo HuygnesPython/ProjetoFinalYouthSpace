d6a0fc364a1b34e401d5ee887372ed1461de8c32
pedrocauan100 <peferrarida@gmail.com>
2026-01-28 19:57:15 -0300

    BETA 0.6.0

--- app.py
+++ app.py
@@ -6,12 +6,11 @@ load_dotenv()
 
 def conectar():
     return my.connect(
-        host=os.getenv("DB_HOST"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-        port=os.getenv("DB_PORT"),
-        database=os.getenv("DB_NAME")
-        
+        host=os.getenv("DB_HOST", "localhost"),
+        user=os.getenv("DB_USER", "root"),
+        password=os.getenv("DB_PASSWORD", ""),
+        port=os.getenv("DB_PORT", "3306"),
+        database=os.getenv("DB_NAME", "PFYS")
     )
 
 app = Flask(__name__)
@@ -21,13 +20,37 @@ def index():
     title = 'Pagina inicial'
     return render_template('index.html', title=title)
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     title = 'Login'
-    return render_template('login.html', title=title)
+    if request.method == 'GET':
+        return render_template('login.html', title=title)
+    if request.method == 'POST':
+        nome = request.form.get('nome')
+        senha = request.form.get('senha')
+        email = request.form.get('email') 
+        conexao = conectar()
+        cursor = conexao.cursor(dictionary=True)
+        sql = "SELECT * FROM usuarios WHERE email = %s"
+        cursor.execute(sql, (email,))
+        resultado = cursor.fetchone()
+        cursor.close()
+        conexao.close()
+        if resultado:
+            if senha == resultado['senha']:
+                print('E-mail e senha corretas')
+                return redirect(url_for('logado'))
+            else:
+                print('Senha Errada')
+                return render_template('login.html', title=title)
+        else:
+            print('E-mail errado')
+            return render_template('login.html', title=title)
+       
 
-
-
+@app.route('/logado')
+def logado():
+    return render_template('logado.html')
 
 
 if __name__ == '__main__':
