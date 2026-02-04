2f012b4efee6536bf63bddedf6301b07dc6b7d9f
HuygnesPython <huygneszindocsgo@gmail.com>
2026-02-04 18:52:01 -0300

    BETA 1.0.0

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def conectar():
     return my.connect(
         host=os.getenv("DB_HOST", "localhost"),
         user=os.getenv("DB_USER", "root"),
-        password=os.getenv("DB_PASSWORD", "neto2303"),
+        password=os.getenv("DB_PASSWORD", "12345"),
         port=os.getenv("DB_PORT", "3306"),
         database=os.getenv("DB_NAME", "PFYS")
     )
