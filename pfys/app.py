f4547bc6e40a2f3ec949f8d1fee80f07bab9f2e2
HuygnesPython <huygnezindocsgo@gmail.com>
2026-02-18 12:22:21 -0300

    V 2.0.0

---
+++ pfys/app.py
@@ -0,0 +1,295 @@
+import os
+from flask import Flask, request, render_template
+import mysql.connector as my
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = Flask(__name__)
+
+def conectar_banco():
+    return my.connect(
+        host=os.getenv("DB_HOST", "localhost"),
+        user=os.getenv("DB_USER", "root"),
+        password=os.getenv("DB_PASSWORD", "neto2303"),
+        database=os.getenv("DB_NAME", "PFYS")
+    )
+
+
+
+
+
+
+@app.route("/", methods=["GET"])
+def index():
+    title = "Página inicial"
+    return render_template("index.html", title=title)
+
+
+
+
+
+
+@app.route("/cadastrar_clientes", methods=["GET", "POST"])
+def cadastrar_clientes():
+    title = "Cadastrar clientes"
+
+    if request.method == "GET":
+        return render_template("cadastrar_clientes.html", title=title)
+
+    if request.method == "POST":
+        nome = request.form.get("nome")
+        email = request.form.get("email")
+        senha_hash = request.form.get("senha_hash")
+        telefone = request.form.get("telefone")
+        documento = request.form.get("documento")
+        endereco = request.form.get("endereco")
+        observacoes = request.form.get("observacoes")
+
+        conexao = conectar_banco()
+        cursor = conexao.cursor()
+
+        sql = "INSERT INTO clientes (nome, email, senha_hash, telefone, documento, endereco, observacoes) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+
+        cursor.execute(sql, (nome, email, senha_hash, telefone, documento, endereco, observacoes))
+        conexao.commit()
+
+        cursor.close()
+        conexao.close()
+
+        return render_template("cadastrar_clientes.html", title=title)
+
+
+
+
+
+
+
+@app.route("/clientes_deletar", methods=["GET", "POST"])
+def deletar_cliente():
+    title = "Deletar clientes"
+    if request.method == "GET":
+        return render_template("clientes_deletar.html")
+
+    id_cliente = request.form.get("id")
+
+    conexao = conectar_banco()
+    cursor = conexao.cursor()
+
+    sql = "DELETE FROM clientes WHERE id = %s"
+    cursor.execute(sql, (id_cliente,))
+    conexao.commit()
+
+    cursor.close()
+    conexao.close()
+
+    return render_template("cadastrar_clientes.html")
+
+
+
+
+
+
+
+@app.route("/cadastrar_veiculos", methods=["GET", "POST"])
+def cadastrar_veiculo():
+    title = "Cadastrar Veículos"
+    if request.method == "GET":
+        conexao = conectar_banco()
+        cursor = conexao.cursor(dictionary=True)
+        sql1 = 'SELECT id, nome FROM clientes'
+        cursor.execute(sql1)
+        resultado = cursor.fetchall()
+        cursor.close()
+        conexao.close()
+        
+        return render_template("cadastrar_veiculos.html", resultado=resultado, title=title)
+    
+    if request.method == "POST":
+        cliente_id = request.form.get("cliente_id")
+        marca = request.form.get("marca")
+        modelo = request.form.get("modelo")
+        placa = request.form.get("placa")
+        ano = request.form.get("ano")
+        observacoes = request.form.get("obs")
+        
+        conexao = conectar_banco()
+        cursor = conexao.cursor()
+
+        sql2 = "INSERT INTO veiculos (cliente_id, marca, modelo, placa, ano, observacoes) VALUES (%s, %s, %s, %s, %s, %s)"
+
+        cursor.execute(sql2, (cliente_id, marca, modelo, placa, ano, observacoes))
+        conexao.commit()
+
+        cursor.close()
+        conexao.close()
+
+        return render_template("cadastrar_veiculos.html", title=title)
+
+
+
+
+
+
+@app.route("/gerenciar_pecas", methods=["GET", "POST"])
+def gerenciar_pecas():
+    title = ""
+    if request.method == "GET":
+        return render_template("gerenciar_pecas.html")
+
+    nome = request.form.get("nome")
+    codigo = request.form.get("codigo")
+    quantidade_estoque = request.form.get("quantidade_estoque")
+    custo = request.form.get("custo")
+    preco_venda = request.form.get("preco_venda")
+    observacoes = request.form.get("observacoes")
+
+    conexao = conectar_banco()
+    cursor = conexao.cursor()
+
+    sql = """
+    INSERT INTO pecas
+    (nome, codigo, quantidade_estoque, custo, preco_venda, observacoes)
+    VALUES (%s, %s, %s, %s, %s, %s)
+    """
+
+    cursor.execute(sql, (nome, codigo, quantidade_estoque, custo, preco_venda, observacoes))
+    conexao.commit()
+
+    cursor.close()
+    conexao.close()
+
+    return render_template("gerenciar_pecas.html", title=title)
+
+
+
+
+
+
+
+
+@app.route("/cadastrar_os", methods=["GET", "POST"])
+def cadastrar_os():
+    title = "Cadastrar Ordem de Servicos"
+    if request.method == "GET":
+        conexao = conectar_banco()
+        cursor = conexao.cursor(dictionary=True)
+        sql1 = 'SELECT id, nome FROM clientes'
+        cursor.execute(sql1)
+        cliente = cursor.fetchall()
+        cursor.close()
+        conexao.close()
+
+        conexao = conectar_banco()
+        cursor = conexao.cursor(dictionary=True)
+        sql2 = 'SELECT id, marca, modelo, placa FROM veiculos'
+        cursor.execute(sql2)
+        veiculo = cursor.fetchall()
+        cursor.close()
+        conexao.close()
+
+        conexao = conectar_banco()
+        cursor = conexao.cursor(dictionary=True)
+        sql3 = 'SELECT id, nome FROM mecanicos'
+        cursor.execute(sql3)
+        mecanico = cursor.fetchall()
+        cursor.close()
+        conexao.close()
+
+        
+
+        return render_template("cadastrar_os.html", cliente=cliente, veiculo=veiculo, mecanico=mecanico, title=title)
+    
+    if request.method == "POST":
+        cliente_id = request.form.get("cliente_id")
+        veiculo_id = request.form.get("veiculo_id")
+        mecanico_id = request.form.get("mecanico_id")
+        problema_relatado = request.form.get("problema_relatado")
+        diagnostico = request.form.get("diagnostico")
+        observacoes = request.form.get("observacoes")
+        status = request.form.get("status")
+        valor_total = request.form.get("valor_total")
+
+        conexao = conectar_banco()
+        cursor = conexao.cursor()
+
+        sql = "INSERT INTO ordem_servicos (cliente_id, veiculo_id, mecanico_id, problema_relatado, diagnostico, observacoes, status, valor_total) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+        
+
+        cursor.execute(sql, (cliente_id, veiculo_id, mecanico_id, problema_relatado, diagnostico, observacoes, status, valor_total))
+
+        conexao.commit()
+
+        cursor.close()
+        conexao.close()
+
+        return render_template("cadastrar_os.html", title=title)
+
+
+@app.route("/cadastrar_mecanico", methods=["GET", "POST"])
+def cadastrar_mecanico():
+    title = "Cadastrar Mecânico"
+    if request.method == "GET":
+        return render_template("cadastrar_mecanico.html")
+
+    nome = request.form.get("nome")
+    especialidade = request.form.get("especialidade")
+    telefone = request.form.get("telefone")
+    observacoes = request.form.get("observacoes")
+
+    conexao = conectar_banco()
+    cursor = conexao.cursor()
+
+    sql = "INSERT INTO mecanicos (nome, especialidade, telefone, observacoes, ativo) VALUES (%s, %s, %s, %s, 1)"
+    
+
+    cursor.execute(sql, (nome, especialidade, telefone, observacoes))
+    conexao.commit()
+
+    cursor.close()
+    conexao.close()
+
+    return render_template("cadastrar_mecanico.html", title=title)
+
+
+
+
+
+
+@app.route("/gerenciar_os", methods=["GET"])
+def gerenciar_os():
+    title = "Gerenciar OS"
+    conexao = conectar_banco()
+    cursor = conexao.cursor(dictionary=True)
+    sql1 = 'SELECT * FROM ordem_servicos'
+    cursor.execute(sql1)
+    ordens = cursor.fetchall()
+    cursor.close()
+    conexao.close()
+
+    if ordens:  
+        conexao = conectar_banco()
+        cursor = conexao.cursor(dictionary=True)
+        sql4 = 'SELECT data_abertura FROM ordem_servicos'
+        cursor.execute(sql4)
+        data = cursor.fetchall()
+        cursor.close()
+        conexao.close()
+        print(data)
+        # conexao = conectar_banco()
+        # cursor = conexao.cursor(dictionary=True)
+        # sql5 = 'SELECT data_prevista_entrega FROM ordem_servicos'
+        # cursor.execute(sql5)
+        # data_entrega = cursor.fetchone()
+        # cursor.close()
+        # conexao.close()
+
+        return render_template("gerenciar_os.html", title=title, ordens=ordens, data=data)
+    
+
+
+    else:
+        return render_template("gerenciar_os.html", title=title)
+    
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ pfys/banco.sql
@@ -0,0 +1,91 @@
+CREATE DATABASE IF NOT EXISTS PFYS;
+USE PFYS;
+
+CREATE TABLE IF NOT EXISTS usuarios (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(150) NOT NULL UNIQUE,
+    senha_hash VARCHAR(255) NOT NULL,
+    ativo TINYINT(1) NOT NULL DEFAULT 1,
+    criado_em DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS clientes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(150) NOT NULL UNIQUE,
+    senha_hash VARCHAR(255) NOT NULL,
+    telefone VARCHAR(20),
+    documento VARCHAR(20),
+    endereco VARCHAR(255),
+    observacoes VARCHAR(250),
+    ativo BOOL DEFAULT 1,
+    criado_em DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS veiculos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    cliente_id INT,
+    marca VARCHAR(99),
+    modelo VARCHAR(99),
+    placa VARCHAR(99),
+    ano VARCHAR(99),
+    obs VARCHAR(99),
+    observacoes VARCHAR(250),
+    ativo BOOL,
+    FOREIGN KEY (cliente_id) REFERENCES clientes(id)
+);
+
+CREATE TABLE IF NOT EXISTS mecanicos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100),
+    especialidade VARCHAR(100),
+    telefone VARCHAR(20),
+    observacoes VARCHAR(250),
+    ativo BOOL DEFAULT 1
+);
+
+CREATE TABLE IF NOT EXISTS pecas (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100),
+    codigo VARCHAR(100),
+    quantidade_estoque INT DEFAULT 0,
+    custo FLOAT,
+    preco_venda FLOAT,
+    observacoes VARCHAR(250),
+    ativo BOOL DEFAULT 1,
+    criado_em DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS ordem_servicos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    cliente_id INT,
+    veiculo_id INT,
+    usuario_abertura_id INT,
+    mecanico_id INT,
+    data_abertura DATETIME DEFAULT CURRENT_TIMESTAMP,
+    data_prevista_entrega DATETIME,
+    data_conclusao DATETIME,
+    status VARCHAR(67),
+    problema_relatado VARCHAR(250),
+    diagnostico VARCHAR(250),
+    observacoes VARCHAR(250),
+    valor_total FLOAT,
+    FOREIGN KEY (cliente_id) REFERENCES clientes(id),
+    FOREIGN KEY (veiculo_id) REFERENCES veiculos(id),
+    FOREIGN KEY (usuario_abertura_id) REFERENCES usuarios(id),
+    FOREIGN KEY (mecanico_id) REFERENCES mecanicos(id)
+);
+
+CREATE TABLE IF NOT EXISTS os_itens (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    os_id INT,
+    peca_id INT,
+    descricao VARCHAR(250),
+    tipo VARCHAR(20),
+    quantidade INT,
+    valor_unitario FLOAT,
+    valor_total FLOAT,
+    FOREIGN KEY (os_id) REFERENCES ordem_servicos(id),
+    FOREIGN KEY (peca_id) REFERENCES pecas(id)
+);
